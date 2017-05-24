08850b32210e0db914bda68d64436399e86df8a0
Anderson Dário <andersonwebwa@hotmail.com>
2017-05-24 15:30:37 -0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+var webpack = require('webpack');
+const Dotenv = require('dotenv-webpack');
+
+module.exports = {
+  entry: [
+    './src/index.js'
+  ],
+  output: {
+    path: __dirname,
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{
+      exclude: /node_modules/,
+      loader: 'babel',
+      query: {
+        presets: ['react', 'es2015', 'stage-1']
+      }
+    }]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './'
+  },
+  plugins: [
+    // Makes some environment variables available to the JS code, for example:
+    // if (process.env.NODE_ENV === 'development') { ... }. See `./env.js`.
+    new Dotenv({
+      path: './.env', // if not simply .env
+      safe: false // lets load the .env.example file as well
+    })
+  ]
+};
